1592262588ed75650e224447a7243f46197f3df6
Christopher Granhof <a24chrgr@student.his.se>
2024-11-22 10:14:30 +0100

    Basic Intro Text Complete

--- Assets/ApplicationHandler.cs
+++ Assets/ApplicationHandler.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 public class ApplicationHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void ChangeScene()
+    public void ChangeToGame()
     {
         SceneManager.LoadSceneAsync("Game Scene", LoadSceneMode.Single);
+    }
+    
+    public void ChangeToMenu()
+    {
+        SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+    }
+    
+    public void ChangeToText()
+    {
+        SceneManager.LoadSceneAsync("IntroText", LoadSceneMode.Single);
     }
 
     public void QuitGame()
